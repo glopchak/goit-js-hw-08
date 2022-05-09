60b0969b1b2f24aa5831c48a8570ca69e4014fa1
Diana Glopchak <dglopchak@gmail.com>
2022-05-09 15:28:53 +0300

    tasks 1, 2, 3 are completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+function galleryItemsMarkup(items){
+return items.map(item => 
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" 
+    src="${item.preview}" 
+    alt="${item.description}" />
+    </a>`).join('')
+};
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup(galleryItems));
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input'),
+};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+const formState = localStorage.getItem('feedback-form-state')
+  ? JSON.parse(localStorage.getItem('feedback-form-state'))
+  : {};
+
+onTetareaInput();
+
+function onFormInput(evt) {
+  formState[evt.target.name] = evt.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+
+  console.log(formState);
+}
+
+function onTetareaInput() {
+  if (!localStorage.getItem('feedback-form-state')) {
+    return;
+  }
+
+  const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+//   console.log(savedMessage);
+
+  if (savedMessage) {
+    if (savedMessage.email) {
+      refs.email.value = savedMessage.email;
+    }
+
+    if (savedMessage.message) {
+      refs.textarea.value = savedMessage.message;
+    }
+  }
+}
